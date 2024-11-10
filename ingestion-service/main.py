3c05c30e84366e5221e1e70db9a0c006f5b32d59
abdeslam272 <abdeslam1999elbennouri@gmail.com>
2024-11-10 15:49:52 +0100

    Adding a Health Check Endpoint to ingestion-service

--- ingestion-service/main.py
+++ ingestion-service/main.py
@@ -5,12 +5,11 @@ import json
 app = FastAPI()
 producer = KafkaProducer(bootstrap_servers='kafka:9092', value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
+@app.get("/health")
+async def health_check():
+    return {"status": "healthy"}
+
 @app.post("/event/")
 async def send_event(event: dict):
     producer.send("clickstream", event)
-    producer.flush()  # Ensure message is sent immediately
     return {"status": "event sent"}
-
-@app.get("/health")
-async def health():
-    return {"status": "healthy"}
