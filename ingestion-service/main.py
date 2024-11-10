149e82f78599abfc0e146e1a3237ff87262acf08
abdeslam272 <abdeslam1999elbennouri@gmail.com>
2024-11-10 15:47:51 +0100

    fixing networking problems and cassandra database is working

--- ingestion-service/main.py
+++ ingestion-service/main.py
@@ -8,4 +8,9 @@ producer = KafkaProducer(bootstrap_servers='kafka:9092', value_serializer=lambda
 @app.post("/event/")
 async def send_event(event: dict):
     producer.send("clickstream", event)
+    producer.flush()  # Ensure message is sent immediately
     return {"status": "event sent"}
+
+@app.get("/health")
+async def health():
+    return {"status": "healthy"}
